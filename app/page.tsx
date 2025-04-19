78a614e78fcd46e84e358909e86d74534f11ddbb
Rajesh562020 <rajesh.mehra56@gmail.com>
2025-04-19 17:37:05 +0530

    fixed lint error on app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from "../app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "../app/ui/home.module.css";
-import InvoiceStatus from "../app/ui/invoices/status";
-import { lusitana,inter } from "./ui/fonts";
+// import styles from "../app/ui/home.module.css";
+// import InvoiceStatus from "../app/ui/invoices/status";
+import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
 export default function Page() {
